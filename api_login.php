7f26e5ca41b22e355bbac35d8cbd45fd6b27be0a
iavtamvan <ade.fajr.ariav@gmail.com>
2019-04-28 23:17:06 +0700

    first commit #1

---
+++ api_login.php
@@ -0,0 +1,80 @@
+<?php
+// mengaktifkan session php
+//session_start();
+
+// menghubungkan dengan koneksi
+include 'config.php';
+
+if ($_POST){
+    // menangkap data yang dikirim dari form
+//    $kode_veriv_email_account = $_POST['kode_veriv_email_account'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+// menyeleksi data admin dengan username dan password yang sesuai
+    $data = mysqli_query($db,"select * from wisba_account where username='$username' and password='$password'");
+//    $data = mysqli_query($db,"select * from account_dasi where kode_veriv_email_account='$kode_veriv_email_account'");
+
+// menghitung jumlah data yang ditemukan
+    $cek = mysqli_num_rows($data);
+    $user = mysqli_fetch_assoc($data);
+
+// $id_user = echo $user['id_user'];
+    $id_account = "".$user['id_account'];
+    $registered = "".$user['registered'];
+    $nama_account = "".$user['nama_account'];
+    $email_account = "".$user['email_account'];
+    $no_hp_account = "".$user['no_hp_account'];
+    $nik_account = "".$user['nik_account'];
+    $alamat_account = "".$user['alamat_account'];
+    $agama_account = "".$user['agama_account'];
+    $jabatan_account = "".$user['jabatan_account'];
+    $kota_account = "".$user['kota_account'];
+    $kab_account = "".$user['kab_account'];
+    $foto_front_account = "".$user['foto_front_account'];
+    $foto_back_account = "".$user['foto_back_account'];
+    $ttd_account = "".$user['ttd_account'];
+    $lat_account = "".$user['lat_account'];
+    $long_account = "".$user['long_account'];
+    $status_account = "".$user['status_account'];
+    if($cek > 0){
+        $response["error"] = false;
+        $response["error_msg"] = "Login Sukses";
+        $response["id_account"] = $id_account;
+        $response["registered"] = $registered;
+        $response["nama_account"] = $nama_account;
+        $response["email_account"] = $email_account;
+        $response["no_hp_account"] = $no_hp_account;
+        $response["nik_account"] = $nik_account;
+        $response["alamat_account"] = $alamat_account;
+        $response["agama_account"] = $agama_account;
+        $response["jabatan_account"] = $jabatan_account;
+        $response["kota_account"] = $kota_account;
+        $response["kab_account"] = $kab_account;
+        $response["foto_front_account"] = $foto_front_account;
+        $response["foto_back_account"] = $foto_back_account;
+        $response["ttd_account"] = $ttd_account;
+        $response["lat_account"] = $lat_account;
+        $response["long_account"] = $long_account;
+        $response["status_account"] = $status_account;
+
+        echo json_encode($response);
+        // echo "Sukses";
+    }else{
+        // header("location:index.php?pesan=gagal");
+//	    header('Location: ../pages/examples/500.html');
+        $response["error"] = true;
+        $response["error_msg"] = "Login Gagal";
+        echo json_encode($response);
+    }
+}
+
+else{
+
+    $response["error"] = true;
+    $response["error_msg"] = "404";
+    echo json_encode($response);
+}
+
+
+?>
---
+++ api_register.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iav
+ * Date: 8/27/18
+ * Time: 12:21 PM
+ */
+
+include("config.php");
+if ($_POST) {
+
+    $nama_account = $_POST['nama_account'];
+    $email_account = $_POST['email_account'];
+    $no_hp_account = $_POST['no_hp_account'];
+//    $nik_account = $_POST['nik_account'];
+//    $alamat_account = $_POST['alamat_account'];
+//    $agama_account = $_POST['agama_account'];
+//    $jabatan_account = $_POST['jabatan_account'];
+//    $kota_account = $_POST['kota_account'];
+//    $kab_account = $_POST['kab_account'];
+    $foto_front_account = $_POST['foto_front_account'];
+    $foto_back_account = $_POST['foto_back_account'];
+    $ttd_account = $_POST['ttd_account'];
+    $lat_account = $_POST['lat_account'];
+    $long_account = $_POST['long_account'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+//    $status_account = $_POST['status_account'];
+
+
+    $sql = "INSERT INTO wisba_account(nama_account, email_account, no_hp_account, foto_front_account, foto_back_account, ttd_account, lat_account, long_account, username, password, status_account) 
+VALUE ('$nama_account', '$email_account', '$no_hp_account', '$foto_front_account', '$foto_back_account', '$ttd_account', '$lat_account', '$long_account', '$username', '$password', 'Umum')";
+
+//    $sql = "INSERT INTO apps_user
+//    (nama_lengkap, email, tempat_tgl_lahir, alamat, kota_kab, provinsi, no_hp, jenis_kelamin, agama, username, password, foto_url, rule, status_verifikasi, token, lat, lng, status_aplikasi, token_firebase)
+//    VALUE
+//    ('$nama_lengkap', '$email', '$tempat_tgl_lahir', '$alamat','$kota_kab', '$provinsi','$no_hp', '$jenis_kelamin', '$agama', '$username', '$password', '$foto_url', '$rule', '$status_verifikasi', '$token', '$lat', '$lng', '$status_aplikasi', '$token_firebase')";
+    $query = mysqli_query($db, $sql);
+
+    // apakah query simpan berhasil?
+    if ($query) {
+        // kalau berhasil alihkan ke halaman index.php dengan status=sukses
+//         header('Location: ../pages/login.html');
+        $response["error"] = false;
+        $response["error_msg"] = "Register Sukses";
+        echo json_encode($response);
+    } else {
+        // kalau gagal alihkan ke halaman indek.php dengan status=gagal
+//         header('Location: ../pages/examples/404.html');
+        $response["error"] = true;
+        $response["error_msg"] = "Register Gagal, username / email telah dipakai : ".$query;
+
+        echo json_encode($response);
+
+    }
+} else {
+    $response["error"] = true;
+    $response["error_msg"] = "404";
+
+    echo json_encode($response);
+}
+?>
